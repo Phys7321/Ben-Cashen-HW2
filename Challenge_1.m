372dc1ce677d3d3e0b61fe1a9b58469151d1ef12
bencashen <43143725+bencashen@users.noreply.github.com>
2018-09-24 14:14:41 -0400

    Add files via upload

--- Challenge_1.m
+++ Challenge_1.m
@@ -1,24 +1,26 @@
 clear;
 
 m = 1;
-a = 2;
-x = linspace(0,a - 0.0001,10000);
+amp = 2;
+x = linspace(0,amp - 0.0001,10000);
 n = length(x);
-f = @(x) sqrt((8*m)./(a.^4 - x.^4));
+f = @(x) sqrt((8*m)./(amp.^4 - x.^4));
 y = f(x);
 t1 = integral(f,x(1),x(n));
 t2 = trapz(x,y);
 fprintf('MATLAB Integral Function: t = %f \nTrapezoid Method: t = %f',t1,t2);
 
-f = @(x,y) sqrt((8*m)./(y.^4 - x.^4));
-y = linspace(0.01,4,10000);
-n = length(y);
+f = @(x,a) sqrt((8*m)./(a.^4 - x.^4));
+a = linspace(0.01,4,10000);
+n = length(a);
 
 t = [];
 
-for i = y(1:n)
+for i = a(1:n)
+x = linspace(0,i - 0.0001,10000);
 t = [t,trapz(x,f(x,i))];
 end
 
-plot(y,t,'-');
-
+plot(a,t,'-');
+xlabel('Amplitude');
+ylabel('Period');
